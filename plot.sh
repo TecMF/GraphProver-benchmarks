f3edac6212cf2890d8015983347a38ee133e0fa8
Guilherme F. Lima <gflima@sdf.org>
2017-07-27 21:35:02 -0300

    maint: Use logscale in all graphs

--- plot.sh
+++ plot.sh
@@ -3,8 +3,7 @@ gnuplot <<EOF
 set title "Time benchmark"
 set xlabel "(|V|+|E|)/2"
 set ylabel "seconds"
-set logscale y
-set pointsize 1.5
+set logscale xy
 set term png
 set output "time.png"
 plot "igraph.time.dat" using 3:(\$1+\$2) with linespoints title "igraph",\
@@ -16,7 +15,7 @@ gnuplot <<EOF
 set title "Memory benchmark"
 set xlabel "(|V|+|E|)/2"
 set ylabel "Kbytes"
-set pointsize 1.5
+set logscale xy
 set term png
 set output "mem.png"
 plot "igraph.mem.dat" using 2:1 with linespoints title "igraph",\
